c55abeaecca0c3ceb5a5fb243a28cb5c800984d2
christianalim <christianalim1@gmail.com>
2019-06-19 14:03:07 -0500

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,77 @@
 # require modules here
+require 'YAML'
+require 'pry'
 
-def load_library
+
+def load_library(path)
   # code goes here
+  library = YAML.load_file("./lib/emoticons.yml")
+  
+  meanings = library.keys
+  emoticons = library.values
+
+  libraryhash = {
+    'get_meaning' => {
+
+    },
+    'get_emoticon' => {
+
+    }
+  }
+
+  meanings.each do |emoticon|
+    emoticons.each do |emotes|
+      libraryhash['get_meaning'][emotes[1]] = nil
+    end
+  end
+
+  count = 0
+  while count < meanings.length do 
+    libraryhash["get_meaning"].each do |emote, definition|  
+      libraryhash["get_meaning"][emote] = meanings[count]
+      count += 1
+    end
+  end
+
+  meanings.each do |emoticon|
+    emoticons.each do |emotes|
+      libraryhash['get_emoticon'][emotes[0]] = nil
+    end
+  end
+
+  japemotes = []
+  emoticons.each do |emotes|
+    japemotes << emotes[1]
+  end 
+
+  counter = 0
+  while counter < japemotes.length do
+    libraryhash["get_emoticon"].each do |amemote, japemote|
+      libraryhash["get_emoticon"][amemote] = japemotes[counter]
+      counter += 1
+    end
+  end
+  
+  libraryhash
+  
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
+  hash = load_library(path)
+  if hash["get_emoticon"][emoticon] == nil
+    "Sorry, that emoticon was not found"
+  else 
+    hash["get_emoticon"][emoticon]
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
+  hash = load_library(path)
+  if hash["get_meaning"][emoticon] == nil
+    "Sorry, that emoticon was not found"
+  else 
+    hash["get_meaning"][emoticon]
+  end
 end
